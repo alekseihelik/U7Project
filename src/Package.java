a031dd753579eb09e6564ddb11d16734c6f91865
BT_4N2_22 <ahelik4984@bths.edu>
2023-02-16 12:31:57 -0500

    finished address class

--- src/Package.java
+++ src/Package.java
@@ -1,2 +1,41 @@
 public class Package {
+    private Address origin;
+    private Address destination;
+    private double pounds;
+    private double length;
+    private double height;
+    private double width;
+
+    public Package(Address origin, Address destination, double pounds, double length, double height, double width){
+        this.origin=origin;
+        this.destination=destination;
+        this.pounds=pounds;
+        this.length=length;
+        this.height=height;
+        this.width=width;
+    }
+
+    public Address getOrigin() {
+        return origin;
+    }
+
+    public Address getDestination() {
+        return destination;
+    }
+
+    public double getPounds() {
+        return pounds;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
 }
